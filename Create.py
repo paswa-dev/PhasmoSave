e63dd466e202814406e455114de1ada9b3b908c6
DirectPaswa <90067145+DirectPaswa@users.noreply.github.com>
2021-09-04 08:08:54 -0400

    Update 2.
    All systems can run now.

---
+++ Create.py
@@ -0,0 +1,85 @@
+from getpass import getpass
+import urllib.request 
+
+
+import os
+import sys
+import requests
+
+area = os.getcwd()
+print(area)
+file_area = f'asset\\Save Crypter.exe'
+path = os.path.join(area, file_area)
+print(path)
+file = path
+
+
+
+relative = os.getcwd()
+folder = 'asset'
+new_relative = f'{relative}/' + folder
+
+sys.path.insert(0, new_relative)
+
+import asset.Finder
+from asset.Finder import searchFile
+from time import sleep
+import subprocess
+import getpass
+user = getpass.getuser()
+
+
+url = 'https://phasmosave.com/saveData.txt'
+
+
+
+fileName2 = 'SaveFile.es3'
+fileName3 = 'SaveFile.es3.tmp.bak'
+
+print('This may take some time. Please be patient')
+
+searchFile('saveData.txt')
+
+joinedFile = asset.Finder.filePATH
+fileArea = os.path.dirname(asset.Finder.filePATH)
+
+delete1 = os.path.join(fileArea, fileName2)
+delete2 = os.path.join(fileArea, fileName3)
+
+if os.path.exists(delete1):
+    print('Found SaveFile.es3') 
+    os.remove(delete1)
+    print('Removed')
+else:
+    print('Skipping Step 2...')
+
+if os.path.exists(delete2):
+    print('Found SaveFile.es3.tmp') 
+    os.remove(delete2)
+    print('Removed')
+else:
+    print('Skipping Step 3...')
+
+if os.path.exists(joinedFile):
+    print('Found old saveData.txt') 
+    os.remove(joinedFile)
+    print('Removing old saveData')
+else:
+    print('Skipping Step 4...')
+
+r = requests.get(url)
+full = r.content
+
+with open(joinedFile, 'wb') as f:
+    f.write(full)
+
+print(f'Wrote file into {fileArea}')
+print('Checking file is there....')
+
+if os.path.exists(joinedFile):
+    print('Success!')
+    subprocess.call([file])
+
+else: 
+    print('Failed?')
+
---
+++ Edit.py
@@ -0,0 +1,49 @@
+import os
+import json
+import getpass
+import subprocess
+from time import sleep
+
+
+user = getpass.getuser()
+
+area = os.getcwd()
+print(area)
+file_area = f'asset\\Save Crypter.exe'
+path = os.path.join(area, file_area)
+print(path)
+file = path
+
+
+def workStart():
+    print('Below is the list of items. Change accordingly.')
+    with open(f"C:\\Users\\{user}\\AppData\\LocalLow\\Kinetic Games\\Phasmophobia\\saveData.txt", "r", encoding="utf-8") as f3:
+        data = json.load(f3)
+        print('')
+        print('')
+        print('If not sure of something CHECK Current stuff above')
+        print('')
+        print('')
+
+    print('Please note that the current level is the max. Do not go above 60 on tools. And keep money below 5k.\n If any value is set as 2, 1, 0 DO NOT CHANGE IT AND SET TO ORIGINAL')
+    for _number in data['IntData']:
+            Number = _number['Value']
+            Name = _number['Key']
+            print(f'Original: {Name}|> {Number}')
+            change = input('| - |  Set :> ')
+            print('\n')
+
+            _number["Value"] = int(change)
+    print('Done')
+    with open(f"C:\\Users\\{user}\\AppData\\LocalLow\\Kinetic Games\\Phasmophobia\\saveData.txt", "w", encoding="utf-8") as f3:
+        json.dump(data , f3)
+        
+    print(data)           
+    sleep(4)
+    print('Enabling Encrypter... DONT CLOSE')
+    subprocess.call([file])
+
+workStart()
+
+
+
