1030924e8a306bacfba216d7e4ac91c7ebf45138
DirectPaswa <90067145+DirectPaswa@users.noreply.github.com>
2021-09-04 21:23:30 -0400

    New change

--- Edit.py
+++ Edit.py
@@ -19,11 +19,11 @@ def workStart():
     print('Below is the list of items. Change accordingly.')
     with open(f"C:\\Users\\{user}\\AppData\\LocalLow\\Kinetic Games\\Phasmophobia\\saveData.txt", "r", encoding="utf-8") as f3:
         data = json.load(f3)
-        print('')
-        print('')
-        print('If not sure of something CHECK Current stuff above')
-        print('')
-        print('')
+    print('')
+    print('')
+    print('If not sure of something CHECK Current stuff above')
+    print('')
+    print('')
 
     print('Please note that the current level is the max. Do not go above 60 on tools. And keep money below 5k.\n If any value is set as 2, 1, 0 DO NOT CHANGE IT AND SET TO ORIGINAL')
     for _number in data['IntData']:
